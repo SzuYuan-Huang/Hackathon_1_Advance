f8cf4d9469ff9e19f7b9970f2e61d4bc01fec9e2
SzuYuan Huang <SzuYuan Huang@SZUYUANHUANG-PC>
2018-03-01 11:41:15 +0800

    OK

--- Hackathon_1_Advance/Program.cs
+++ Hackathon_1_Advance/Program.cs
@@ -13,6 +13,7 @@ namespace Hackathon_1_Advance
             for(int i=1;i<=100;i++)
             {
                 Console.WriteLine(i.ToString().Replace("3", "A").Replace("5","B").Replace("9","C").Replace("0","D"));
+
             }
             Console.ReadLine();
         }
